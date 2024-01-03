5f30c5539c1fae957e8376907a357a451312974f
Amusah <henryamusah@gmail.com>
2024-01-03 18:35:52 +0000

    reverse geocode current location

--- app.js
+++ app.js
@@ -3,40 +3,60 @@ import Utilities from './utils.js';
 
 
 const navBar = document.querySelector('.navbar');
+
 // const navToggler = navBar.querySelector('.nav-toggler');
 
 const utils = new Utilities();
 
 class App {
+  #apiKeys = {
+    weatherApi : 'TJJ852M4XXSA9Y5XRWETDERQK',
+    geoCodeApi : '788221805148350681091x86622'
+  }
+  currentCity;
+
   constructor(){
     navBar.addEventListener('click', utils.toggleNav);
   }
 
-  getCurrentLocation(){
-    if(!navigator.geolocation) return
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
+  fetchAndParseData(url, errMsg = 'Something went wrong'){
+    return fetch(url).then(res => {
+      if(!res.ok){
+        utils.throwError(errMsg, res.status);
+        return;
+      }
+      return res.json();
+    })
+  }
 
-    const getCoordinates = pos => {
+  getCurrentLocation(){
+    if(!navigator.geolocation) return;
+
+    navigator.geolocation.getCurrentPosition(pos => {
       console.log(pos)
       const { latitude, longitude } = pos.coords;
       console.log(latitude, longitude);
-    }
-
-    const error = err => {
-      // console.log(err)
-      alert(`Could not get your current location, please enable your device's location `);
-    }
-
-    navigator.geolocation.getCurrentPosition(getCoordinates, error);
+      // Reverse Geocoding
+      this.fetchAndParseData(`https://geocode.xyz/${latitude},${longitude}?geoit=json&auth=${this.#apiKeys.geoCodeApi}`)
+      .then(data => {
+        console.log(data.city)
+      })
+      
+    }, (err) => {
+      if(err)
+      utils.throwError('Location not found', 'Enable device\'s location');
+    });
   }
+
+  getWeatherForcast(){}
 
 }
 
 const weatherApp = new App();
-weatherApp.getCurrentLocation()
+weatherApp.getCurrentLocation();
 // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Takoradi?unitGroup=metric&key=TJJ852M4XXSA9Y5XRWETDERQK&contentType=json
 // 'TJJ852M4XXSA9Y5XRWETDERQK'
+
+// goecode api
+// '788221805148350681091x86622'
+// 'https://geocode.xyz/51.50354,-0.12768?geoit=xml&auth=your_api_key'
--- utils.js
+++ utils.js
@@ -9,5 +9,19 @@ export default class Utilities{
       navBar.classList.toggle('scale-nav');
     }
   }
+
+  showSpinner(){
+    const spinner = document.querySelector('.loading');
+  }
+
+  throwError(msg1, msg2){
+    const messageBox = document.querySelector('.msg-box');
+    const html = `
+      <h2 class="msg-head">${msg1}</h2>
+      <p class="msg">${msg2 ? msg2 : ''}</p>
+    `;
+    messageBox.insertAdjacentHTML('beforeend', html);
+    messageBox.classList.toggle('hidden');
+  }
   
 }
